85bd8373ec0c55af1cded89ea8f61cbcb802934c
Ahmed Abuelgasim <hungryphilomath@gmail.com>
2019-11-20 13:12:04 +0000

    split code into smaller chunks #8

--- scripts/markdown-html.mjs
+++ scripts/markdown-html.mjs
@@ -1,7 +1,13 @@
 import fs from 'fs';
 import MarkdownIt from 'markdown-it';
 
-const pa11yCompsDir = './src/pa11y/components';
+const libraryName = 'pa11y';
+const componentsDir = `./src/${libraryName}/components`;
+
+const magentaString = '\x1b[35m%s\x1b[0m';
+const greenString = '\x1b[32m%s\x1b[0m';
+// const cyanString = '\x1b[36m%s\x1b[0m';
+// const yellowString = '\x1b[93m%s\x1b[0m';
 
 
 // MarkdownIt Options
@@ -12,145 +18,122 @@ const md = new MarkdownIt({
 });
 
 
+
 // HELPER FUNCTIONS
+// Replace Content between given indices
 const replaceContentBetweenIndices = (sourceString, stringToInsert, startIndex, endIndex) => {
   const substr1 = sourceString.substr(0, startIndex);
   const substr2 = sourceString.substr(endIndex);
   return `${substr1}${stringToInsert}${substr2}`;
 }
 
+// Write content to file
+const writeContentToFile = (mdContent, filePath) => {
+  console.log(magentaString, `>> Saving changes to ${filePath}`);
+  fs.writeFileSync(filePath, mdContent);
+  // CATCH
+  console.log(greenString, `>> Changes to ${filePath} saved successfully`);
+}
+
+
+// INJECT SASS CODE INTO MARKDOWN
+const injectSass = (componentName, mdFileContents, writeToReadme=false) => {
+  const componentDir = `${componentsDir}/${componentName}`;
+  const sassFilePath = `${componentDir}/_${componentName}.scss`
+
+  // Read scss file
+  console.log(magentaString, `>> Reading ${sassFilePath}`);
+  const sassFileContents = fs.readFileSync(sassFilePath).toString();
+  // CATCH
+
+  // Inject sassFileContents into mdFileContents between "```scss" and "```"
+  const startQuery = '```scss', endQuery = '```';
+  const queryIndex = mdFileContents.indexOf(startQuery);
+  const startIndex = queryIndex + startQuery.length + 1;
+  const endIndex = mdFileContents.indexOf(endQuery, startIndex);
+  const newMdFileContents = replaceContentBetweenIndices(mdFileContents, sassFileContents, startIndex, endIndex);
+
+  if (writeToReadme) {
+    console.log(magentaString, `>> Injecting _${componentName}.scss code into README.md`);
+    writeContentToFile(newMdFileContents, `${componentDir}/README.md`);
+  }
+
+  return newMdFileContents;
+}
+
+
+// INJECT HTML CODE INTO MARKDOWN AND CONVERT MARKDOWN TO
+const injectHtmlAndConvertMd = (componentName, mdFileContents) => {
+  const componentDir = `${componentsDir}/${componentName}`;
+  const examplesDirPath = `${componentDir}/examples`;
+  const mdFilePath = `${componentDir}/README.md`; //
+  const startQuery = '```html', endQuery = '```';
+  let queryIndex, startIndex, endIndex;
+  let mdFromIndex = 0;
+  let htmlFromIndex = 0;
+  let mdToHtmlSource = mdFileContents;
+
+  console.log(magentaString, `>> Injecting html examples code into ${componentDir}/README.md`);
+  const exampleFiles = fs.readdirSync(`${examplesDirPath}`);
+  // CATCH
+
+  exampleFiles.forEach(file => {
+    console.log(magentaString, `>> Reading ${examplesDirPath}/${file}`);
+    const exampleFileContents = fs.readFileSync(`${examplesDirPath}/${file}`).toString();
+    // CATCH
+
+    // Inject example HTML code into code fence in md source
+    queryIndex = mdFileContents.indexOf(startQuery, mdFromIndex);
+    startIndex = queryIndex + startQuery.length + 1;
+    endIndex = mdFileContents.indexOf(endQuery, startIndex);
+    console.log(magentaString, `>> Injecting ${file} HTML into ${componentDir}/README.md`);
+    mdFileContents = replaceContentBetweenIndices(mdFileContents, exampleFileContents, startIndex, endIndex);
+    mdFromIndex = startIndex;
+
+    // Inject example HTML code into mdToHtmlSource
+    queryIndex = mdToHtmlSource.indexOf(startQuery, htmlFromIndex);
+    startIndex = queryIndex + startQuery.length + 1;
+    endIndex = mdToHtmlSource.indexOf(endQuery, startIndex);
+    mdToHtmlSource = replaceContentBetweenIndices(mdToHtmlSource, exampleFileContents, startIndex, endIndex);
+    console.log(magentaString, `>> Injecting ${file} HTML into ${componentName}.html`);
+    mdToHtmlSource = replaceContentBetweenIndices(mdToHtmlSource, exampleFileContents, queryIndex - 1, queryIndex - 1);
+    htmlFromIndex = startIndex + exampleFileContents.length;
+  });
+
+  writeContentToFile(mdFileContents, mdFilePath);
+  convertMdToHtmlAndSave(mdToHtmlSource, componentName);
+}
+
+// CONVERT MARKDOWN CODE TO HTML AND SAVE TO HTML FILE
+const convertMdToHtmlAndSave = (mdSource, componentName) => {
+  const htmlFilePath = `./src/${componentName}/index.html`;
+  console.log(magentaString, `>> Converting markdown source to ${htmlFilePath}`);
+  let convertedHtml = md.render(mdSource);
+  writeContentToFile(convertedHtml, htmlFilePath);
+}
+
 
 // Per component function
-const mdToHtml = componentName => {
-  const componentDir = `${pa11yCompsDir}/${componentName}`;
-  const mdFilePath = `${componentDir}/README.md`;
-  const htmlFilePath = `./src/${componentName}/index.html`;
-  const sassFilePath = `${componentDir}/_${componentName}.scss`;
-  const examplesDirPath = `${componentDir}/examples`;
-
-  const magentaString = '\x1b[35m%s\x1b[0m';
-  const greenString = '\x1b[32m%s\x1b[0m';
-  const cyanString = '\x1b[36m%s\x1b[0m';
-  // const yellowString = '\x1b[93m%s\x1b[0m';
+const injectSassAndHtmlCode = componentName => {
+  const componentDir = `${componentsDir}/${componentName}`;
+  const mdFilePath = `${componentDir}/README.md`; //
 
   // Read md file
   console.log(magentaString, `>> Reading ${componentDir}/README.md`);
   let mdFileContents = fs.readFileSync(mdFilePath).toString();
+  // CATCH
 
-
-  // INJECT SASS CODE INTO MARKDOWN
-  // Read scss file
-  console.log(magentaString, `>> Reading ${componentDir}/_${componentName}.scss`);
-  let sassFileContents = fs.readFileSync(sassFilePath).toString();
-
-  // Inject sassFileContents into mdFileContents and write to file
-  console.log(magentaString, `>> Injecting scss code into _${componentName}.scss`);
-
-  let startQuery = '```scss';
-  const endQuery = '```';
-  const queryIndex = mdFileContents.indexOf(startQuery);
-  const startIndex = queryIndex + startQuery.length + 1;
-  const endIndex = mdFileContents.indexOf(endQuery, startIndex);
-  mdFileContents = replaceContentBetweenIndices(mdFileContents, sassFileContents, startIndex, endIndex);
-
-
-  // INJECT HTML CODE INTO MARKDOWN AND CONVERT TO HTML
-  console.log(magentaString, `>> Injecting html examples code into ${componentDir}/README.md`);
-  startQuery = '```html';
-  let mdFromIndex = 0;
-  let htmlFromIndex = 0;
-  let mdToHtmlSource = '';
-
-  const exampleFilesArray = fs.readdirSync(`${examplesDirPath}`);
-
-
-  // Inject example HTML code into code fences in md source
-  exampleFilesArray.forEach(file => {
-    console.log(magentaString, `>> Reading ${examplesDirPath}/${file}`);
-    const exampleFileContents = fs.readFileSync(`${examplesDirPath}/${file}`).toString();
-
-    let queryIndex = mdFileContents.indexOf(startQuery, mdFromIndex);
-    const startIndex = queryIndex + startQuery.length + 1;
-    const endIndex = mdFileContents.indexOf(endQuery, startIndex);
-
-    console.log(magentaString, `>> Injecting ${file} code into code fence in ${componentDir}/README.md`);
-    mdFileContents = replaceContentBetweenIndices(mdFileContents, exampleFileContents, startIndex, endIndex);
-    mdFromIndex = startIndex;
-    // console.log(cyanString, mdFileContents);
-
-    // Save changes to md file
-    console.log(magentaString, `>> Saving changes to ${componentName}/README.md`);
-    fs.writeFileSync(mdFilePath, mdFileContents);
-    console.log(greenString, `>> ${componentName}/README.md saved successfully`);
-  });
-
-
-  // Inject example HTML code to md source before converting to HTML
-  exampleFilesArray.forEach(file => {
-    const exampleFileContents = fs.readFileSync(`${examplesDirPath}/${file}`).toString();
-    console.log(magentaString, `>> Injecting ${file} into ${componentName}.html`);
-    let queryIndex = mdFileContents.indexOf(startQuery, htmlFromIndex);
-    mdFileContents = replaceContentBetweenIndices(mdFileContents, exampleFileContents, queryIndex - 1,  queryIndex - 1);
-    htmlFromIndex = queryIndex + exampleFileContents.length + 1;
-  });
-
-  // convert markdown to HTML
-  let convertedHtml = md.render(mdFileContents);
-  console.log(magentaString, `>> Saving changes to ${htmlFilePath}`);
-  fs.writeFileSync(htmlFilePath, convertedHtml);
-  console.log(greenString, `>> ${htmlFilePath} saved successfully`);
+  mdFileContents = injectSass(componentName, mdFileContents);
+  injectHtmlAndConvertMd(componentName, mdFileContents);
 }
 
 
-// For all components in pa11yCompsDir
-fs.readdirSync(pa11yCompsDir, { withFileTypes: true })
+
+// For all component directories in componentsDir
+fs.readdirSync(componentsDir, { withFileTypes: true })
   .filter(item => item.isDirectory())
   .map(directory => {
     // For each directory
-    mdToHtml(directory.name);
+    injectSassAndHtmlCode(directory.name);
   });
-
-
-
-///////////////////////////////////////
-// mdToHtml('test-component');
-// const injectExamples = directory => {
-//   JSDOM.fromFile(`./src/${directory}/index.html`)
-//     .then(dom => {
-//       const document = dom.window.document
-//       const exampleBlocks = document.querySelectorAll('.example-block');
-//       let fileContentss = null;
-
-//       fs.readdirSync(`${pa11yCompsDir}/${directory}/examples/`)
-//         .forEach((file, i) => {
-//           try {
-//             fileContentss = fs.readFileSync(`${pa11yCompsDir}/${directory}/examples/${file}`, 'utf8');
-//           } catch(err) {
-//             console.error(err);
-//           }
-
-//           exampleBlocks[i].innerHTML = fileContentss;
-//           const newContents = dom.serialize();
-
-//           try {
-//             fs.writeFileSync(`./src/${directory}/index.html`, newContents)
-//             console.log(magentaString, `>> Injecting examples into ${directory}/index.html`);
-//           } catch(err) {
-//             console.error(err);
-//           }
-//         });
-
-//       console.log('>>> Examples injection done');
-//     });
-// }
-
-// const insertBetweenStrings = (sourceString, startStr, endStr, stringToInsert) => {
-//   const newString = `${startStr}${stringToInsert}${endStr}`;
-//   var rx = new RegExp(`${startStr}[\\d\\D]*?${endStr}`);
-//   return sourceString.replace(rx, newString);
-// }
-
-// try {
-// } catch(err) {
-//   console.error(err);
-// }
+// CATCH
