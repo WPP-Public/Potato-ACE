49c21cdb2fc05e9fccf0c7b19e32d482052a19bf
alex.eales <alex.eales@potatolondon.com>
2019-11-29 09:30:14 +0000

    Fixed disclosure test and CI test job

--- src/asce/components/disclosure/disclosure.test.js
+++ src/asce/components/disclosure/disclosure.test.js
@@ -1,29 +1,49 @@
 context('Disclosure', () => {
     beforeEach(() => {
+        // Navigate to the docs page
         cy.visit('/disclosure');
     });
 
     // CHECK DOCS PAGE IS CORRECT BEFORE TESTING EXAMPLES
     describe('Docs Page', () => {
+        // Store the package name from environment for use later
+        const pkgName = Cypress.env('pkg_name');
+
         it('should have at least one button trigger', () => {
-            cy.get(`button[asce-disclosure-trigger-for]`).should('have.length.greaterThan', 0);
+            cy.get(`button[${pkgName}-disclosure-trigger-for]`).should('have.length.greaterThan', 0);
         });
 
         it('should have at least one non-button trigger', () => {
-            cy.get(`:not(button)[asce-disclosure-trigger-for]`).should('have.length.greaterThan', 0);
+            cy.get(`:not(button)[${pkgName}-disclosure-trigger-for]`).should('have.length.greaterThan', 0);
         });
 
         it('should have at least one disclosure', () => {
-            cy.get(`[asce-disclosure]`).should('have.length.greaterThan', 0);
+            cy.get(`${pkgName}-disclosure`).should('have.length.greaterThan', 0);
         });
     });
 
     // TEST EXAMPLES AGAINST WAI-ARIA SPEC
     describe('WAI-ARIA Spec', () => {
-        it('should trigger disclosure when enter is pressed', () => {
-            const trigger = document.querySelector('[asce-disclosure-trigger-for]');
-            const disclosure = document.getElementById(trigger.getAttribute('asce-disclosure-trigger-for'));
+        // Store the package name from environment for use later
+        const pkgName = Cypress.env('pkg_name');
 
+        it('triggers should have role of button', () => {
+            cy.get(`[${pkgName}-disclosure-trigger-for]`).should('have.attr', 'role', 'button');
+        });
+
+        it('should trigger disclosure when clicked', () => {
+            // Get the trigger and disclosure
+            cy.get(`[${pkgName}-disclosure-trigger-for]`).first().as('trigger');
+            cy.get('@trigger').then((trigger) => {
+                const disclosureId = trigger.attr(`${pkgName}-disclosure-trigger-for`);
+                cy.get(`#${disclosureId}`).as('disclosure');
+                // Check disclosure is hidden before
+                cy.get('@disclosure').should('have.attr', 'style', 'display: none;');
+                // Click the trigger
+                cy.get('@trigger').wait(150).click();
+                // Check disclosure is now visible and trigger has `aria-expanded` set to true
+                cy.get('@disclosure').should('have.not.have.attr', 'style', 'display: none;');
+            });
         });
     });
 });
