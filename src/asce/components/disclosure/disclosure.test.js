27619e6a3b4c2715f2640ff6184fb7f12e14a0cf
alex.eales <alex.eales@potatolondon.com>
2019-11-29 15:00:52 +0000

    Added tests for aria correctness

--- src/asce/components/disclosure/disclosure.test.js
+++ src/asce/components/disclosure/disclosure.test.js
@@ -27,10 +27,6 @@ context('Disclosure', () => {
         // Store the package name from environment for use later
         const pkgName = Cypress.env('pkg_name');
 
-        it('triggers should have role of button', () => {
-            cy.get(`[${pkgName}-disclosure-trigger-for]`).should('have.attr', 'role', 'button');
-        });
-
         it('should trigger disclosure when clicked', () => {
             // Get the trigger and disclosure
             cy.get(`[${pkgName}-disclosure-trigger-for]`).first().as('trigger');
@@ -42,8 +38,46 @@ context('Disclosure', () => {
                 // Click the trigger
                 cy.get('@trigger').wait(150).click();
                 // Check disclosure is now visible and trigger has `aria-expanded` set to true
-                cy.get('@disclosure').should('have.not.have.attr', 'style', 'display: none;');
+                cy.get('@disclosure').should('not.have.attr', 'style', 'display: none;');
             });
+        });
+
+        // TODO: Work out how to trigger button with enter/space
+        // it('should trigger disclosure when enter is pressed', () => {
+        //     // Get the trigger and disclosure
+        //     cy.get(`[${pkgName}-disclosure-trigger-for]`).first().as('trigger');
+        //     cy.get('@trigger').then((trigger) => {
+        //         const disclosureId = trigger.attr(`${pkgName}-disclosure-trigger-for`);
+        //         cy.get(`#${disclosureId}`).as('disclosure');
+        //         // Check disclosure is hidden before
+        //         cy.get('@disclosure').should('have.attr', 'style', 'display: none;');
+        //         // Focus the trigger
+        //         cy.get('@trigger').then(el => {
+        //             el.focus();
+        //             el.trigger('keypress', { keycode: 13, which: 13 });
+        //         });
+        //         // Check disclosure is now visible and trigger has `aria-expanded` set to true
+        //         cy.get('@disclosure').should('not.have.attr', 'style', 'display: none;');
+        //     });
+        // });
+
+        it('triggers should have role of button', () => {
+            cy.get(`[${pkgName}-disclosure-trigger-for]`).should('have.attr', 'role', 'button');
+        });
+
+        it('trigger should have aria-controls set to id of disclosaure', () => {
+            // Get the trigger and disclosure
+            cy.get(`[${pkgName}-disclosure-trigger-for]`).first().as('trigger');
+            cy.get('@trigger').then((trigger) => {
+                const disclosureId = trigger.attr(`${pkgName}-disclosure-trigger-for`);
+                cy.get('@trigger').should('have.attr', 'aria-controls', disclosureId);
+            });
+        });
+
+        it('trigger should have aria-expanded set to true when disclosure visible', () => {
+            // Get the trigger and disclosure
+            cy.get(`[${pkgName}-disclosure-trigger-for]`).first().as('trigger');
+            cy.get('@trigger').click().wait(150).should('have.attr', 'aria-expanded', 'true');
         });
     });
 });
