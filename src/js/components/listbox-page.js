6dc3dc77740480e9ab23d272b04afd141374c442
Ahmed Abuelgasim <hungryphilomath@gmail.com>
2019-11-29 12:13:10 +0000

    added script injection into injection engine #1

---
+++ src/js/components/listbox-page.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import { Listbox, CONSTS as LISTBOX_CONSTS } from '../asce/components/listbox/listbox.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Testing listbox dynamic
+  document.getElementById('dynamic-listbox-btn')
+    .addEventListener('click', () => {
+      const selectId = 'dynamic-listbox';
+      const listbox = document.getElementById(selectId);
+      const list = listbox.querySelector('ul');
+      list.innerHTML = `
+        <li>Iron Man</li>
+        <li>Nick Fury</li>
+        <li>Hulk</li>
+        <li>Black Widow</li>
+        <li>Thor</li>
+        <li>Captain America</li>
+        <li>Scarlet Witch</li>
+        <li>Ant-Man</li>
+        <li>Spider-man</li>
+        <li>Black Panther</li>
+        <li>Doctor Strange</li>
+        <li>Captain Marvel</li>`;
+
+      listbox.dispatchEvent(
+        new CustomEvent(
+          `${LISTBOX_CONSTS.UPDATE_OPTIONS_EVENT}`,
+          { detail: { id: selectId } },
+        )
+      );
+    });
+
+});
