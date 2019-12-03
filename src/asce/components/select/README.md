bcfbf6889f1deb1f57c73bee3b1b882bc1d81270
Ahmed Abuelgasim <hungryphilomath@gmail.com>
2019-12-03 17:48:30 +0000

    added select component #4

---
+++ src/asce/components/select/README.md
@@ -0,0 +1,18 @@
+# Select
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.	
+
+[W3C Spec](https://www.w3.org/TR/wai-aria-practices-1.1/examples/listbox/listbox-collapsible.html)
+
+## Usage
+
+```js
+import { Select } from 'asce/components/select/select.js';
+```
+
+
+## Examples
+
+### Select Drop-down
+
+```html
+```
---
+++ src/asce/components/select/select.js
@@ -0,0 +1,147 @@
+/* IMPORTS */
+import { libraryName, KEYBOARD_KEYS as KEYS } from '../../common/constants.js';
+import { Listbox, ATTRS as LISTBOX_ATTRS } from '../listbox/listbox.js';
+import { keyPressedMatches } from '../../common/common.js';
+
+
+/* CONSTANTS */
+export const NAME = `${libraryName}-select`;
+
+export const ATTRS = {
+  LIST: `${NAME}-list`,
+  TRIGGER: `${NAME}-trigger`,
+};
+
+
+/* CLASS */
+export class Select extends Listbox {
+  /* CONSTRUCTOR */
+  constructor() {
+    super();
+
+    /* CLASS INSTANCE CONSTANTS */
+
+    /* GET DOM ELEMENTS */
+    this.trigger = this.querySelector('button');
+    this.list = this.querySelector('ul');
+    this.label = this.querySelector('label');
+
+    /* GET DOM DATA */
+
+    /* BIND 'THIS' TO CLASS METHODS */
+    this.selectClickHandler = this.selectClickHandler.bind(this);
+    this.selectKeydownHandler = this.selectKeydownHandler.bind(this);
+    this.showList = this.showList.bind(this);
+    this.hideList = this.hideList.bind(this);
+  }
+
+
+  /* CLASS METHODS */
+  connectedCallback() {
+    Listbox.prototype.connectedCallback.call(this);
+
+    /* ATTACH EVENT LISTENERS */
+    window.addEventListener('click', this.selectClickHandler, { passive: true });
+    this.addEventListener('keydown', this.selectKeydownHandler);
+    this.list.addEventListener('blur', this.hideList, { passive: true });
+
+
+    /* SET INITIAL STATES */
+    // Set label attrs
+    let triggerLabel = this.trigger.id;
+    if (this.label) {
+      this.label.id = this.label.id || `${this.id}-label`;
+      triggerLabel = `${this.label.id} ${triggerLabel}`;
+    }
+
+    // Set trigger attrs
+    this.trigger.id = this.trigger.id || `${this.id}-trigger`;
+    this.trigger.setAttribute(ATTRS.TRIGGER, '');
+
+    if (!this.getAttribute('aria-haspopup')) {
+      this.trigger.setAttribute('aria-haspopup', 'listbox');
+    }
+
+    if (!this.getAttribute('aria-labelledby')) {
+      this.trigger.setAttribute('aria-labelledby', triggerLabel);
+    }
+
+    // Set list attrs
+    this.list.setAttribute(ATTRS.LIST, '');
+    this.list.setAttribute('tabindex', '-1');
+
+    /* INITIALISATION CODE */
+    this.hideList();
+  }
+
+
+  /*
+    Handle clicks on trigger and on listbox options
+  */
+  selectClickHandler(e) {
+    const optionClicked = e.target.closest(`[${LISTBOX_ATTRS.OPTION_INDEX}]`);
+    const triggerClicked = e.target.closest(`[${ATTRS.TRIGGER}]`);
+
+    if (!optionClicked && !triggerClicked) {
+      return;
+    }
+
+    if (triggerClicked) {
+      this.showList();
+      return;
+    }
+
+    this.hideList();
+  }
+
+
+  /*
+    Handle keystrokes
+  */
+  selectKeydownHandler(e) {
+    const keydownOnTrigger = e.target.closest(`[${ATTRS.TRIGGER}]`);
+    const keydownOnList = e.target.closest(`[${LISTBOX_ATTRS.LIST}]`);
+
+    if (!keydownOnTrigger && !keydownOnList) {
+      return;
+    }
+
+    const keyPressed = e.key || e.which || e.keyCode;
+    if (keydownOnTrigger && keyPressedMatches(keyPressed, [KEYS.UP, KEYS.DOWN])) {
+      this.showList();
+      Listbox.prototype.keydownHandler.call(this, e);
+      return;
+    }
+
+    if (keydownOnList && keyPressedMatches(keyPressed, [KEYS.ENTER, KEYS.ESCAPE])) {
+      e.preventDefault();
+      this.hideList();
+      this.trigger.focus();
+      return;
+    }
+  }
+
+
+  /*
+    Show dropdown list
+  */
+  showList() {
+    this.list.style.display = null;
+    this.list.focus();
+  }
+
+
+  /*
+    Hide dropdown list and update trigger text
+  */
+  hideList() {
+    const activeOption = this.list.querySelector(`[aria-selected="true"]`);
+    if (activeOption !== null) {
+      this.trigger.textContent = activeOption.textContent;
+    }
+
+    this.list.style.display = 'none';
+  }
+}
+
+customElements.define(NAME, Select);
